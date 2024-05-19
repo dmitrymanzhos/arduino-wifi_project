43abd3351d256f927048c2f334836e2f5fb48512
dm218 <manzhos.ds@phystech.edu>
2024-05-20 02:26:18 +0300

    5 commit

--- ino/osnovnoy.ino
+++ ino/osnovnoy.ino
@@ -6,6 +6,19 @@
 
 #include <stdio.h>
 #include <SoftwareSerial.h> // если UNO
+
+#define frequency 115200
+
+#define SSID  "Pppppp"
+#define PSW "2444666668888888"
+
+#define SERVER_IP "192.168.43.214"
+#define SERVER_PORT 7000
+#define ESP_PORT 7001
+
+#define BUFFER_SIZE 512
+char buffer[BUFFER_SIZE];
+
 SoftwareSerial esp(2, 3);
  
 // #define esp Serial1 // меняем Serial1, на esp, ибо serial может быть софтовый
@@ -40,35 +53,55 @@ SoftwareSerial esp(2, 3);
 // }  
 ///////////
 
+//void do_command(char* command){
+//  while(1){
+//    esp.println(command);
+//    if (esp.find("OK")) break;
+//    else Serial.println("err");
+//    delay(100);
+//  }
+//  return;
+//}
+
+
+
 void setup() {
-  Serial.begin(115200); // Serial monitor
-  esp.begin(115200); // ESP8266
- 
+  Serial.begin(frequency); // Serial monitor
+  esp.begin(frequency); // ESP8266
+  
+  clearSerialBuffer();
+  Serial.println(GetResponse("AT+RST",3400));
+
+  Serial.println(GetResponse("AT+CWMODE=3",300));
+  connectWiFi();
+  // Serial.println(GetResponse("AT+CIPMUX=0",300));
   // while (!Serial); // wait for serial port to connect. Needed for Leonardo only
-  esp.println("AT+RST");
-  esp.println("AT");
-  while (esp.available()) { // смотрим что ответил есп после отправки
-    Serial.write(esp.read()); 
-  }
-  esp.println("AT+CWMODE=1");     // делаем есп клиентом  
+  // esp.println("AT+RST");
+  // esp.println("AT");
+  // while (esp.available()) { // смотрим что ответил есп после отправки
+  //   Serial.write(esp.read()); 
+  // }
+  // esp.println("AT+CWMODE=3");     // делаем есп клиентом  
   
   // Serial.write((char)esp.read());
   // while (esp.available() > 0) { // смотрим что ответил есп после отправки
     
   //   Serial.write(esp.read()); 
   // }
-  esp.println("AT+RST"); // сбрасываем есп   
-  delay(500);   
-  esp.println("AT+CWJAP=\"Pppppp\",\"2444666668888888\""); // подключение к сети
+//   esp.println("AT+RST"); // сбрасываем есп   
+//   delay(500);   
+//   esp.println("AT+CWJAP=SSID,PWD); // подключение к сети
+// //  esp.println("AT+CWJAP=\"&Dolgopniki\",\"KSP_kr0t0vuxa\"");
 
-  esp.println("AT+CIPMUX=0");     // выключаем режим множественных подключений  
+//   esp.println("AT+CIPMUX=0");     // выключаем режим множественных подключений  
   
-  esp.flush();
-
+//   esp.flush();
+//
   while(1){                       // ждем подключение к сети
     esp.println("AT+CWJAP?");     // спрашиваем состояние
     // while (esp.available() > 0){
       // Serial.write(esp.read());
+    
     if (esp.find("OK")) break;  // если в ответе на команду нашли OK, выходим из цикла
     else {
       Serial.print("-");
@@ -76,11 +109,13 @@ void setup() {
     }
     delay(250);    
   }
-
+//  do_command("AT+CWJAP?");
+  
   Serial.println("GOOOAL");
 
   while(1){                       // ждем пока выдадут IP
     esp.println("AT+CIFSR");       // спрашиваем состояние
+    // break;
     if (esp.find("192")) break;  // если нам дали IP, выходим из цикла
     else Serial.print("net");
     delay(250);    
@@ -94,13 +129,128 @@ void setup() {
  
 
 void loop() {
-  esp.println("AT+CIPSTART=\"TCP\",\"192.168.43.1\",443");
-  esp.println("AT+CIPSEND=0,124");
-  esp.println("write 10.20.30.40 5.623");
-  delay(1000);
-  while (esp.available() > 0) { // смотрим что ответил есп после отправки
-    Serial.write(esp.read());
-    // Serial.println("printed\n"); 
+  delay(300);
+  while(1){
+    esp.println("AT+CIPSTART=\"UDP\",\"192.168.43.214\",7000,7001,0");
+    delay(100);
+    if (esp.find("CON")){
+      Serial.println("goooal");
+      break;
+    }
+    else Serial.println("err");
+  }
+//  esp.println("AT+CIPSTART=\"UDP\",\"192.168.43.1\",7000,7001,0");
+//  esp.println("AT+CIPSTART=\"TCP\",\"192.168.43.1\",7000,7000,0");
+//  do_command("AT+CIPSTART=\"UDP\",\"192.168.43.1\",7000,7001,0");
+  
+//  delay(500);
+//  esp.println("AT+CIPMODE=1");
+//  delay(500);
+
+
+  while(1){
+    esp.println("AT+CIPSEND=25");
+    delay(100);
+    if (esp.find("OK")){
+      Serial.println("goooal.1");
+      break;
+    }
+    else Serial.println("err");
+  }
+//  esp.println("AT+CIPSEND=0,31");
+//  do_command("AT+CIPSEND=0,31");
+
+
+//  delay(500);
+  while(1){
+    esp.println("write 10.20.30.40 5.623");
+    delay(100);
+    if (esp.find("SEND")){
+      clearSerialBuffer();
+      Serial.println("goooal.2");
+      break;
+    }
+    else {
+      Serial.println("err");
+      clearSerialBuffer();
+      return loop();
+    }
   }
 
+//  esp.println("write 10.20.30.40 5.623");
+//  delay(500);
+
+  
+//   esp.println("AT+CIPMODE=0");
+//   delay(500);
+
+//    while(1){
+//    esp.println("AT+CIPCLOSE");
+//    delay(100);
+//    if (esp.find("CLOSED")){
+//      Serial.println("goooal.3");
+//      break;
+//    }
+//    else Serial.println("err");
+//  }
+  
+//  esp.println("AT+CIPCLOSE");
+  delay(500);
+  
+  Serial.println("sent\n");
+  while (esp.available() > 0) { // смотрим что ответил есп после отправки
+    Serial.write(esp.read());
+//     Serial.println("printed\n"); 
+  }
+  delay(1000);
+}
+
+/////////////////////отправка АТ-команд/////////////////////
+String GetResponse(String AT_Command, int wait)
+{
+  String tmpData;
+  
+  esp.println(AT_Command);
+  delay(wait);
+  while (esp.available() >0 )  
+   {
+    char c = esp.read();
+    tmpData += c;
+    
+    // if ( tmpData.indexOf(AT_Command) > -1 )         
+    //   tmpData = "";
+    // else
+    //   tmpData.trim();       
+          
+   }
+  return tmpData;
+}
+
+////////////////////подключение к wifi/////////////////////        
+boolean connectWiFi() 
+{
+  String cmd = "AT+CWJAP=\"";
+  cmd += SSID;
+  cmd += "\",\"";
+  cmd += PSW;
+  cmd += "\"";
+  Serial.println(cmd); 
+  Serial.println(GetResponse(cmd,6500));
+}
+
+//////////////////////очистка esp////////////////////
+void clearSerialBuffer(void) 
+{
+       while ( esp.available() > 0 ) 
+       {
+         esp.read();
+       }
+}
+
+////////////////////очистка буфера//////////////////////// 
+void clearBuffer(void) {
+       for (int i =0;i<BUFFER_SIZE;i++ ) 
+       {
+         buffer[i]=0;
+       }
 }
